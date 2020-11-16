8ca677481d93c70faf10f9fe1b436b785a9eb39a
Robert Schmahl <rschm007@gmail.com>
2020-11-16 12:25:39 -0800

    new example readme

--- Develop/ExampleREADME.md
+++ Develop/ExampleREADME.md
@@ -1,66 +1,61 @@
-undefined
 
-    ## Project Title
-    -----------------------------
-    ## Description
-    -----------------------------
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam
-    ## Table of Contents
-    * [Installation](#installation)
-    
-    * [Usage](#usage)
-    
-    * [Contribute](#contribute)
-    
-    * [Tests](#tests)
-    
-    * [Credits](#credits)
-    
-    * [License](#license)
-    
-    
-    ## Installation
-    -----------------------------
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-    
-    
-    
-    ## Usage
-    -----------------------------
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-    
-    
-  
-    ## Contributing
-    -----------------------------
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+## Project Title
+-----------------------------
+## Description
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+## Table of Contents
+* [Installation](#installation)
 
-    
-    ## Tests
-    -----------------------------
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+* [Usage](#usage)
 
-    
-    ## Acknowledgements
-    -----------------------------
-    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+* [Contribute](#contribute)
 
-    
-    ## License
-    -----------------------------
-    MIT
+* [Tests](#tests)
 
-    
-    -----------------------------
-    ### Questions or comments?
-    -----------------------------
-    <img src="https://avatars1.githubusercontent.com/u/69170803?v=4" alt="A profile pic of the developer"></img>
-    
-    If you'd like to contact the developer, please use the info below:
-    
-    GitHub:
-    
-    * @rschm007 
-    
-    * https://api.github.com/users/rschm007
-    
+* [Credits](#credits)
+
+* [License](#license)
+
+## Installation
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+
+
+## Usage
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+
+
+## Contributing
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+
+
+## Tests
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+
+
+## Acknowledgements
+-----------------------------
+Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+
+
+## License
+-----------------------------
+MIT
+
+
+-----------------------------
+### Questions or comments?
+-----------------------------
+<img src="https://avatars1.githubusercontent.com/u/69170803?v=4" alt="A profile pic of the developer"></img>
+
+If you'd like to contact the developer, please use the info below:
+
+GitHub:
+
+* @rschm007 
+
+* https://api.github.com/users/rschm007
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,138 +1,150 @@
-let draftMarkdown;
+let draftMarkdown = '';
 
 // function to generate markdown for README
 function generateMarkdown(userResp, userInfo) {
   // We will base the markdown from the Good README directions in 01-HTML
 
   // Title section
-    draftMarkdown += `
-
-    ## ${userResp.title}
-    -----------------------------
-    ## Description
-    -----------------------------
-    ${userResp.description}
-    `;
+    draftMarkdown += 
+`
+## ${userResp.title}
+-----------------------------
+## Description
+-----------------------------
+${userResp.description}
+`;
 
   // generate table of contents
-  let ToC = `## Table of Contents`;
+  let ToC = 
+`## Table of Contents`;
   // installation ToC
   if (userResp.installation !== "") {
-    ToC += `
-    * [Installation](#installation)
-    `};
+    ToC += 
+`
+* [Installation](#installation)
+`};
 
   // usage ToC
   if (userResp.usage !== "") {
-    ToC += `
-    * [Usage](#usage)
-    `};
+    ToC += 
+`
+* [Usage](#usage)
+`};
 
   // contribute ToC
   if (userResp.contribute !== "") {
-    ToC += `
-    * [Contribute](#contribute)
-    `};
+    ToC += 
+`
+* [Contribute](#contribute)
+`};
 
   // tests ToC
   if (userResp.tests !== "") {
-    ToC += `
-    * [Tests](#tests)
-    `};
+    ToC += 
+`
+* [Tests](#tests)
+`};
 
   // credit ToC
   if (userResp.contribute !== "") {
-    ToC += `
-    * [Credits](#credits)
-    `};
+    ToC += 
+`
+* [Credits](#credits)
+`};
 
   // license ToC
   if (userResp.contribute !== "") {
-    ToC += `
-    * [License](#license)
-    `};
+    ToC += 
+`
+* [License](#license)
+`};
 
   draftMarkdown += ToC;
 
   // Installation section
-  if (userResp.installation != "") {
-    draftMarkdown += `
-    
-    ## Installation
-    -----------------------------
-    ${userResp.installation}
-    
-    `};
+  if (userResp.installation !== "") {
+    draftMarkdown += 
+`
+## Installation
+-----------------------------
+${userResp.installation}
+
+`};
 
   // add Usage section
-  if (userResp.usage != "") {
-    draftMarkdown += `
-    
-    ## Usage
-    -----------------------------
-    ${userResp.usage}
-    
-    `};
+  if (userResp.usage !== "") {
+    draftMarkdown += 
+`
+## Usage
+-----------------------------
+${userResp.usage}
+
+`};
 
   // contributing section
-  if (userResp.contribute != "") {
-    draftMarkdown += `
-  
-    ## Contributing
-    -----------------------------
-    ${userResp.contribute}
+  if (userResp.contribute !== "") {
+    draftMarkdown += 
+`
+## Contributing
+-----------------------------
+${userResp.contribute}
 
-    `};
+`};
 
   // tests section
-  if (userResp.tests != "") {
-    draftMarkdown += `
-    ## Tests
-    -----------------------------
-    ${userResp.tests}
+  if (userResp.tests !== "") {
+    draftMarkdown += 
+`
+## Tests
+-----------------------------
+${userResp.tests}
 
-    `};
+`};
 
   // credit section
-  if (userResp.credits != "") {
-    draftMarkdown += `
-    ## Acknowledgements
-    -----------------------------
-    ${userResp.credits}
+  if (userResp.credits !== "") {
+    draftMarkdown += 
+`
+## Acknowledgements
+-----------------------------
+${userResp.credits}
 
-    `};
+`};
 
   // License section
-  if (userResp.license != "") {
-  draftMarkdown += `
-    ## License
-    -----------------------------
-    ${userResp.license}
+  if (userResp.license !== "") {
+  draftMarkdown += 
+`
+## License
+-----------------------------
+${userResp.license}
 
-    `};
+`};
 
   // Developer Section
-  let devSection = `
-    -----------------------------
-    ### Questions or comments?
-    -----------------------------
-    <img src="${userInfo.avatar_url}" alt="A profile pic of the developer"></img>
-    
-    If you'd like to contact the developer, please use the info below:
-    
-    GitHub:
-    
-    * @${userInfo.login} 
-    
-    * ${userInfo.url}
-    `;
+  let devSection = 
+`
+-----------------------------
+### Questions or comments?
+-----------------------------
+<img src="${userInfo.avatar_url}" alt="A profile pic of the developer"></img>
+
+If you'd like to contact the developer, please use the info below:
+
+GitHub:
+
+* @${userInfo.login} 
+
+* ${userInfo.url}
+`;
 
   // if github email is valid, add it
   if (userInfo.email !== null) {
-    devSection += `
-    
-    Email: ${userInfo.email}
-    `};
+    devSection += 
+`
+
+* Email: ${userInfo.email}
+`};
 
   // add dev section to markdown
   draftMarkdown += devSection;
