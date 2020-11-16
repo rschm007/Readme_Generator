8f1c4f074a4729e733f34d7bf3ef80a254fdb23a
Robert Schmahl <rschm007@gmail.com>
2020-11-15 18:38:05 -0800

    trying to make the app generate new lines in the readme

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -4,133 +4,65 @@ const { doesNotMatch } = require("assert");
 function generateMarkdown(userResp, userInfo) {
   // We will base the markdown from the Good README directions in 01-HTML
   // Generate table of contents based on userResp
-  let tableOfContents = `# Table of Contents`;
-    // define the table of contents sections based on what the user inputs
-    if (userResp.installation !== '') { tableOfContents += `
-    <br>
-    Installation
-    <br>
-    `};
+  let tableOfContents = `Table of Contents`;
+  // define the table of contents sections based on what the user inputs
+  if (userResp.installation !== "") {
+    tableOfContents += `<br>Installation<br>`;
+  }
 
-    if (userResp.usage !== '') { tableOfContents += `
-    <br>
-     [Usage](#usage)
-    <br>
-    `};
+  if (userResp.usage !== "") {
+    tableOfContents += `<br>Usage<br>`;
+  }
 
-    if (userResp.contribute !== '') { tableOfContents += `
-    <br>
-    Contributing
-    <br>
-    `};
+  if (userResp.contribute !== "") {
+    tableOfContents += `<br>Contributing<br>`;
+  }
 
-    if (userResp.tests !== '') { tableOfContents += `
-    <br>
-    Tests
-    <br>
-    `};
+  if (userResp.tests !== "") {
+    tableOfContents += `<br>Tests<br>`;
+  }
 
-    if (userResp.credits !== '') { tableOfContents += `
-    <br>
-    Credits
-    <br>
-    `};
+  if (userResp.credits !== "") {
+    tableOfContents += `<br>Credits<br>`;
+  }
 
-    let draftMarkdown = 
-    `
-    <br>
-    ${userResp.title}
-    <br>
-    Description
-    <br>
-    -----------------------------
-    <br>
-    ${userResp.description}
-    <br>`;
+  let draftMarkdown = `<br>${userResp.title}<br>Description<br>-----------------------------<br>${userResp.description}<br>`;
 
-    // add Table of Contents
-    draftMarkdown += tableOfContents;
+  // add Table of Contents
+  draftMarkdown += tableOfContents;
 
-    // add License section
-    draftMarkdown += `
-    <br>
-    License
-    <br>
-    -----------------------------
-    <br>
-    `;
+  // add License section
+  draftMarkdown += `<br>License<br>-----------------------------<br>`;
 
-    // Installation section
-    if (userResp.installation !== '') {
-      draftMarkdown += `
-      <br>
-      Installation
-      <br>
-      -----------------------------
-      <br>
-      ${userResp.installation}
-      <br>
-      `};
+  // Installation section
+  if (userResp.installation !== "") {
+    draftMarkdown += `<br>Installation<br>-----------------------------<br>${userResp.installation}<br>`;
+  }
 
-    // add Usage section
-    if (userResp.usage !== '') {
-      draftMarkdown += `
-      <br>
-      Usage
-      <br>
-      -----------------------------
-      <br>
-      ${userResp.usage}
-      <br>
-      `};
+  // add Usage section
+  if (userResp.usage !== "") {
+    draftMarkdown += `<br>Usage<br>-----------------------------<br>${userResp.usage}<br>`;
+  }
 
-    // contributing section
-    if (userResp.contribute !== '') {
-      draftMarkdown += `
-      <br>
-      Contributing
-      <br>
-      -----------------------------
-      <br>
-      ${userResp.contribute}
-      <br>
-      `};
+  // contributing section
+  if (userResp.contribute !== "") {
+    draftMarkdown += `<br>Contributing<br>-----------------------------<br>${userResp.contribute}<br>`;
+  }
 
-    // tests section
-    if (userResp.tests !== '') {
-      draftMarkdown+= `
-      <br>
-      Tests
-      <br>
-      -----------------------------
-      <br>
-      ${userResp.tests}
-      <br>
-      `};
+  // tests section
+  if (userResp.tests !== "") {
+    draftMarkdown += `<br>Tests<br>-----------------------------<br>${userResp.tests}<br>`;
+  }
 
-    // credit section
-    if (userResp.credits !== '') {
-      draftMarkdown+= `
-      Acknowledgements
-      <br>
-      -----------------------------
-      <br>
-      ${userResp.credits}
-      <br>
-      `};
+  // credit section
+  if (userResp.credits !== "") {
+    draftMarkdown += `Acknowledgements<br>-----------------------------<br>${userResp.credits}<br>`;
+  }
 
-    // License section
-    draftMarkdown+= `
-    <br>
-    License
-    <br>
-    -----------------------------
-    <br>
-    ${userResp.license}
-    <br>
-    `;
+  // License section
+  draftMarkdown += `<br>License<br>-----------------------------<br>${userResp.license}<br>`;
 
-    return draftMarkdown;
-};
+  return draftMarkdown;
+}
 
 module.exports = generateMarkdown;
--- README.md
+++ README.md
@@ -4,43 +4,20 @@ Readme Generator
 
 Links
 -----------------------------
-Deployed <a href="https://rschm007.github.io/Portfolio_Page/html/index.html">https://rschm007.github.io/Portfolio_Page/html/index.html</a>
-<br>
-Repository <a href="https://github.com/rschm007/Portfolio_Page">https://github.com/rschm007/Portfolio_Page</a>
+
+Repository <a href="https://github.com/rschm007/Readme_Generator">https://github.com/rschm007/Readme_Generator</a>
 
 Installation
 -----------------------------
 
-To install this application you will need a text editor like Visual Studio Code. There are several NPM packages which you should be able to install with "npm install" in your CLI. You will also need to install the Tailwindcss framework (https://tailwindcss.com/), PUG HTML compiler (https://pugjs.org/api/getting-started.html), and Sass CSS compiler (https://sass-lang.com/).
-
+To install this application you will need a text editor like Visual Studio Code. You will also need node.js to type "npm install" in your CLI to install inquirer and assert packages
 
 Features
 -----------------------------
-This site was developed with Tailwindcss, Pug and Sass. It incorporates a grid system to create a responsive layout. Items that would normally display in multiple columns should display as a single column on smaller screens.
+This node app uses the CLI to ask the user a series of questions in order to generate a professional readme.md draft.
 
 <img src="https://raw.githubusercontent.com/rschm007/Portfolio_Page/master/assets/imgs/portfolio_gif-1.gif" alt="Robert Schmahl Portfolio gif" style="max-width:100%;">
 
-Other features include:
-<ul>
-  <li>Navbar links to navigate between content pages</li>
-  <li>Page render animations to enhance the UX</li>
-  <li>Brief biography and picture of me</li>
-  <li>Portfolio page that contains technical projects with animated gifs and links to deployed apps and Github repos</li>
-  <li>Functional contact page with Google Form that saves form content, as well as social media buttons</li>
-  <li>Downloadable resume PDF</li>
-</ul>
-
-Planned Future Features
------------------------------
-<ul>
-  <li>Enhanced mobile responsiveness & media queries</li>
-  <li>Links to more technical projects as well as graphic design projects</li>
-  <li>Tidy up animation functions</li>
-  <li>About Me needs copy editing, as it ends rather weakly right now</li>
-  <li>Current resume is one of my outdated marketing-specific resumes. This needs to be changed.</li>
-  <li>Migrate contact form from Google Forms to custom form</li>
-  <li>Refactor code for DRY and upload portfolio to my own server and hosted URL</li>
-</ul>
 
 Authors
 -----------------------------
@@ -52,7 +29,5 @@ Acknowledgements
 -----------------------------
 <ul>
   <li>UC Irvine Web Development program for providing project requirements and code examples</li>
-  <li>Wilmai at codepen.io for this awesome page render animation: <a href="https://codepen.io/wilmai/pen/MvejPb">https://codepen.io/wilmai/pen/MvejPb</a></li>
-  <li>Chad Selph for the jQuery typewriter I forked: <a href="https://github.com/chadselph/jquery-typewriter">https://github.com/chadselph/jquery-typewriter</a></li>
 </ul>
 
